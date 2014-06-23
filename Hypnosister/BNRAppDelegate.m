517e67b389ad7df521deaad548ef8d2229f4eb95
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-24 00:55:30 +0700

    Try to render rectangle programmatically.

--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -7,12 +7,20 @@
 //
 
 #import "BNRAppDelegate.h"
+#import "BNRHypnosisView.h"
 
 @implementation BNRAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+    
+    BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
+    firstView.backgroundColor = [UIColor redColor];
+    [self.window addSubview:firstView];
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
