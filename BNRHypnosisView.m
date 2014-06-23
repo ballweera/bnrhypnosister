c40ce6b370696216cf73a8b60fe5897927aa5d95
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-24 01:35:24 +0700

    Repeat draw circle by add new one to one instance of UIBezier.

--- BNRHypnosisView.m
+++ BNRHypnosisView.m
@@ -33,14 +33,14 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    // The circle will be the largest that will fir in the view
-    float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+    float maxRadius = hypot(bounds.size.width, bounds.size.height / 2.0);
     
     UIBezierPath *path = [[UIBezierPath alloc] init];
     
-    // Add an arc to the path at center, with radius of radius
-    // from 0 to 2*PI radians (a circle)
-    [path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        [path addArcWithCenter:center radius:currentRadius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
+    }
     
     // Configure line width to 10 points
     path.lineWidth = 10;
