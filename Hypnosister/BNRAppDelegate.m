f23802e4048cf4c422f677583a775ecd26421ebe
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-24 01:01:51 +0700

    Add second view (blue one) to UIWindow

--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -21,6 +21,12 @@
     firstView.backgroundColor = [UIColor redColor];
     [self.window addSubview:firstView];
     
+    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
+    
+    BNRHypnosisView *secondView = [[BNRHypnosisView alloc] initWithFrame:secondFrame];
+    secondView.backgroundColor = [UIColor blueColor];
+    [self.window addSubview:secondView];
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
